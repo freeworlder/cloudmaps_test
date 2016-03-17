4a62c4c27093dea6d53861e57cb5f94e16d6a7da
Andrey Shevtsov <freeworlder@gmail.com>
2016-03-17 20:52:14 +0100

    Добавлена возможность просматривать профили пользователей. Если вручную вбить имя пользователя, который не является другом, то выдаётся ошибка.

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -8,106 +8,131 @@ var crypto = require('crypto');
 
 module.exports = {
 
-  register: function(req, res){
-    if(req.method == 'POST'){
+  register: function (req, res) {
+    if (req.method == 'POST') {
       var model = req.allParams();
       model.password = crypto.createHash('sha256').update(model.password).digest('hex');
       delete model.id;
-      User.create(model, function(error, data){
-        if(error){
+      User.create(model, function (error, data) {
+        if (error) {
           res.view('user/error', {message: 'При регистрации пользователя произошла ошибка: ' + error.message});
         }
-        else{
+        else {
           // if there are no errors, send verification email
-          sendActEmail(req,res,data)
+          sendActEmail(req, res, data)
         }
       });
     }
-    else if(req.method == 'GET'){
-      if(req.param('id') && req.param('t')){
+    else if (req.method == 'GET') {
+      if (req.param('id') && req.param('t')) {
         var id = parseInt(req.param('id')),
-            token = req.param('t');
-        User.findOne(id).exec(function(error,user){
-          if(error){
-            res.view('user/error',{message: 'При активации пользователя произошла ошибка: ' + error.message});
+          token = req.param('t');
+        User.findOne(id).exec(function (error, user) {
+          if (error) {
+            res.view('user/error', {message: 'При активации пользователя произошла ошибка: ' + error.message});
           }
-          else{
-            if(user.password == token){
-              User.update(id, {active: true}).exec(function(error){
-                if(error){
-                  res.view('user/error',{message: 'При активации пользователя произошла ошибка: ' + error.message});
+          else {
+            if (user.password == token) {
+              User.update(id, {active: true}).exec(function (error) {
+                if (error) {
+                  res.view('user/error', {message: 'При активации пользователя произошла ошибка: ' + error.message});
                 }
-                else{
+                else {
                   res.redirect('/login');
                 }
               });
             }
-            else{
-              res.view('user/error',{message: 'При активации пользователя произошла ошибка: неверный ключ активации'});
+            else {
+              res.view('user/error', {message: 'При активации пользователя произошла ошибка: неверный ключ активации'});
             }
           }
         });
       }
-      else{
+      else {
         res.view();
       }
     }
   },
 
-  login: function(req, res){
-    if(req.method == 'POST'){
-      User.findOne({username: req.param('username')}).exec(function(error, user){
-        if(error){
-          res.view('user/error',{message: 'При проверке логина и пароля произошла ошибка: ' + error.message});
+  login: function (req, res) {
+    if (req.method == 'POST') {
+      User.findOne({username: req.param('username')}).exec(function (error, user) {
+        if (error) {
+          res.view('user/error', {message: 'При проверке логина и пароля произошла ошибка: ' + error.message});
         }
-        else{
+        else {
           // TODO check if a user if found otherwise there is a null error
-          if(user.password == crypto.createHash('sha256').update(req.param('password')).digest('hex')){
+          if (user.password == crypto.createHash('sha256').update(req.param('password')).digest('hex')) {
             req.session.user = user;
-            return res.redirect('/user/'+user.username);
+            return res.redirect('/user/' + user.username);
           }
-          else{
-            res.view('user/error',{message: 'Неверный логин или пароль'});
+          else {
+            res.view('user/error', {message: 'Неверный логин или пароль'});
           }
         }
       });
     }
-    else{
-      if(typeof req.session.user == 'undefined'){
+    else {
+      if (typeof req.session.user == 'undefined') {
         return res.view();
       }
-      else{
-        return res.redirect('/user/'+req.session.user.username);
+      else {
+        return res.redirect('/user/' + req.session.user.username);
       }
     }
   },
 
-  profile: function(req, res){
-    User.findOne({username: req.param('username')}).exec(function(error, user){
-      if(error){
-        res.view('user/error',{message: 'Ошибка: ' + error.message});
+  profile: function (req, res) {
+    // making a list of friends ids
+    var friends_ids = [];
+    Friend.find({id_user: req.session.user.id}).exec(function (error, friends) {
+      if (error) {
+        return res.negotiate(error);
       }
-      else{
-        res.view({
-          user: _.omit(user, 'password')
+      else {
+        friends_ids = _.map(friends, function (friend) {
+          return friend.id_friend;
         });
+      }
+    });
+    User.findOne({username: req.param('username')}).exec(function (error, user) {
+      if (error) {
+        res.view('user/error', {message: 'Ошибка: ' + error.message});
+      }
+      else {
+        //showing profile only of self or one of the friends.
+        if (user.id == req.session.user.id || friends_ids.indexOf(user.id)>-1) {
+          res.view({
+            user: _.omit(user, 'password'),
+            current_user: _.omit(req.session.user, 'password')
+          });
+        }
+        // If requested profile of a non-friend user, showing error
+        else {
+          res.view('user/error', {
+            message: 'Вы не можете просматривать профили пользователей, ' +
+            'которые не являются вашими друзьями.'
+          })
+        }
       }
     });
   },
 
-  friends: function(req, res){
-    if(req.xhr){
-      switch(req.method){
+  friends: function (req, res) {
+    if (req.xhr) {
+      switch (req.method) {
         case 'GET':
-          User.findOne(parseInt(req.param('id', 0))).populate('friends').exec(function(error, user){
-            if(error)
+          User.findOne(parseInt(req.param('id', 0))).populate('friends').exec(function (error, user) {
+            if (error)
               return res.negotiate(error);
             else {
-              var friend_ids = _.map(user.friends, function(friend){return friend.id_friend;});
-              User.find(friend_ids).exec(function(error, friends){
-                if(error)
+              var friend_ids = _.map(user.friends, function (friend) {
+                return friend.id_friend;
+              });
+              User.find(friend_ids).exec(function (error, friends) {
+                if (error)
                   return res.negotiate(error);
-                else{
+                else {
                   return res.view({friends: friends});
                 }
               });
@@ -119,12 +144,12 @@ module.exports = {
           Friend.destroy({
             id_user: [id, req.session.user.id],
             id_friend: [id, req.session.user.id]
-          }).exec(function(error){
-            if(error){
+          }).exec(function (error) {
+            if (error) {
               return res.negotiate(error);
             }
-            else{
-              sails.sockets.blast('delete_friend',{
+            else {
+              sails.sockets.blast('delete_friend', {
                 id_user: req.session.user.id,
                 id_friend: id
               });
@@ -136,23 +161,27 @@ module.exports = {
           return res.badRequest();
       }
     }
-    else{
+    else {
       return res.badRequest();
     }
   },
 
-  requests: function(req, res){
-    if(req.xhr){
-      switch(req.method){
+  requests: function (req, res) {
+    if (req.xhr) {
+      switch (req.method) {
         case 'GET':
           Request.find({
             id_requested: parseInt(req.param('id', 0))
-          }).populate('id_requesting').exec(function(error, requests){
-            if(error){
+          }).populate('id_requesting').exec(function (error, requests) {
+            if (error) {
               return res.negotiate(error);
             }
             else {
-              return res.view({requests: _.map(requests, function(request){return request.id_requesting;})});
+              return res.view({
+                requests: _.map(requests, function (request) {
+                  return request.id_requesting;
+                })
+              });
             }
           });
           break;
@@ -160,21 +189,21 @@ module.exports = {
           Friend.create([{
             id_user: req.session.user.id,
             id_friend: parseInt(req.param('id'))
-          },{
+          }, {
             id_friend: req.session.user.id,
             id_user: parseInt(req.param('id'))
-          }]).exec(function(error, data){
-            if(error)
+          }]).exec(function (error, data) {
+            if (error)
               return res.negotiate(error);
-            else{
+            else {
               Friend.publishCreate(data[0], req);
               Request.destroy({
                 id_requesting: parseInt(req.param('id')),
                 id_requested: req.session.user.id
-              }).exec(function(error){
-                if(error)
+              }).exec(function (error) {
+                if (error)
                   return res.negotiate(error);
-                else{
+                else {
                   return res.ok();
                 }
               });
@@ -185,10 +214,10 @@ module.exports = {
           Request.destroy({
             id_requesting: parseInt(req.param('id')),
             id_requested: req.session.user.id
-          }).exec(function(error){
-            if(error)
+          }).exec(function (error) {
+            if (error)
               return res.negotiate(error);
-            else{
+            else {
               return res.ok();
             }
           });
@@ -197,35 +226,35 @@ module.exports = {
           return res.badRequest();
       }
     }
-    else{
+    else {
       return res.badRequest();
     }
   },
 
-  avatar: function(req, res){
+  avatar: function (req, res) {
     var fs = require('fs');
     var avatar_dir = sails.config.rootPath + '/avatars/';
-    if(req.method == 'GET'){
+    if (req.method == 'GET') {
       var avatar = avatar_dir + req.param('id') + '.jpg';
-      fs.stat(avatar, function(error, stats){
-        if(error){
+      fs.stat(avatar, function (error, stats) {
+        if (error) {
           return res.sendfile(avatar_dir + 'default-avatar.jpg');
         }
-        else if(stats.isFile()){
+        else if (stats.isFile()) {
           return res.sendfile(avatar);
         }
-        else{
+        else {
           return res.notFound();
         }
       });
     }
-    else if(req.method == 'POST'){
-      req.file('file').upload({}, function(error, files){
-        if(error)
+    else if (req.method == 'POST') {
+      req.file('file').upload({}, function (error, files) {
+        if (error)
           return res.negotiate(error);
-        else{
-          fs.rename(files[0].fd, avatar_dir+req.session.user.id+'.jpg', function(error){
-            if(error)
+        else {
+          fs.rename(files[0].fd, avatar_dir + req.session.user.id + '.jpg', function (error) {
+            if (error)
               return res.negotiate(error);
             else
               return res.ok();
@@ -236,26 +265,30 @@ module.exports = {
     }
   },
 
-  logout: function(req, res){
+  logout: function (req, res) {
     delete req.session.user;
     return res.redirect('/');
   },
 
-  list: function(req, res){
-    if(req.xhr){
-      Friend.find({id_user: req.session.user.id}).exec(function(error, friends){
-        if(error)
+  list: function (req, res) {
+    if (req.xhr) {
+      Friend.find({id_user: req.session.user.id}).exec(function (error, friends) {
+        if (error)
           return res.negotiate(error);
-        else{
-          var exclude = _.map(friends, function(friend){return friend.id_friend;});
-          Request.find({id_requesting: req.session.user.id}).exec(function(error, requests){
-            if(error)
+        else {
+          var exclude = _.map(friends, function (friend) {
+            return friend.id_friend;
+          });
+          Request.find({id_requesting: req.session.user.id}).exec(function (error, requests) {
+            if (error)
               return res.negotiate(error);
-            else{
-              exclude = exclude.concat(_.map(requests, function(request){return request.id_requested;}));
+            else {
+              exclude = exclude.concat(_.map(requests, function (request) {
+                return request.id_requested;
+              }));
               exclude.push(req.session.user.id);
-              User.find({id: {'!': exclude}}).exec(function(error, list){
-                if(error)
+              User.find({id: {'!': exclude}}).exec(function (error, list) {
+                if (error)
                   return res.negotiate(error);
                 else {
                   return res.view({list: list});
@@ -266,42 +299,42 @@ module.exports = {
         }
       });
     }
-    else{
+    else {
       return res.badRequest();
     }
   },
 
-  subscribe: function(req, res){
-    if(req.isSocket && req.session.user){
+  subscribe: function (req, res) {
+    if (req.isSocket && req.session.user) {
       Request.watch(req);
       Friend.watch(req);
     }
     return res.ok();
   },
 
-  request: function(req, res){
-    if(req.xhr){
+  request: function (req, res) {
+    if (req.xhr) {
       var id_requested = req.param('id_requested');
       Request.count({
         id_requesting: req.session.user.id,
         id_requested: id_requested
-      }).exec(function(error, count){
-        if(error)
+      }).exec(function (error, count) {
+        if (error)
           return res.negotiate(error);
-        else{
-          if(!count){
+        else {
+          if (!count) {
             Request.create({
               id_requesting: req.session.user.id,
               id_requested: id_requested
-            }).exec(function(error, request){
-              if(error){
+            }).exec(function (error, request) {
+              if (error) {
                 return res.send({
                   success: false,
                   error: error
                 });
               }
-              else{
-                Request.findOne(request.id).populateAll().exec(function(error, request){
+              else {
+                Request.findOne(request.id).populateAll().exec(function (error, request) {
                   request.id_requesting = _.omit(request.id_requesting, 'password');
                   Request.publishCreate(request, req);
                   return res.send({
@@ -312,7 +345,7 @@ module.exports = {
               }
             });
           }
-          else{
+          else {
             return res.send({
               success: true,
               message: "Заявка уже существует"
@@ -321,23 +354,23 @@ module.exports = {
         }
       });
     }
-    else{
+    else {
       return res.badRequest();
     }
   },
 
   //this view send activation email
   sendActivationEmail: function (req, res) {
-      // checking if a user logged in before getting here. If so, req.session.user contains user object
-      if (typeof req.session.user != 'undefined') {
-        model = req.session.user;
-      }
-      // we are here only in case of direct request of the controller without previous login as a non-active user
-      // or registration process. If so, the funtion just doesn't know what user should it send email to.
-      else {
-        res.view('user/error', {message: 'При отправке письма произошла ошибка: невозможно определить пользователя.'});
-      }
-      sendActEmail(req,res,model)
+    // checking if a user logged in before getting here. If so, req.session.user contains user object
+    if (typeof req.session.user != 'undefined') {
+      model = req.session.user;
+    }
+    // we are here only in case of direct request of the controller without previous login as a non-active user
+    // or registration process. If so, the funtion just doesn't know what user should it send email to.
+    else {
+      res.view('user/error', {message: 'При отправке письма произошла ошибка: невозможно определить пользователя.'});
+    }
+    sendActEmail(req, res, model)
   }
 };
 
@@ -345,7 +378,7 @@ module.exports = {
 // has to be separate function for the code to be DRY, as it could be called
 // 1) when a user is created,
 // 2) or when a  user requests to send a verification letter one more time
-function sendActEmail (req, res, model) {
+function sendActEmail(req, res, model) {
   var nodemailer = require('nodemailer');
   var smtpTransport = require('nodemailer-smtp-transport');
   var transporter = nodemailer.createTransport(smtpTransport({
@@ -354,15 +387,15 @@ function sendActEmail (req, res, model) {
       ignoreTLS: true
     })
   );
-  var mailOptions ={
-    from: 'test@cloudmaps.ru' ,
+  var mailOptions = {
+    from: 'test@cloudmaps.ru',
     to: model.email,
     subject: 'User Activation Email',
     // TODO: generate verification tocken, not to use password (even encrypted)
-    text: 'http://localhost:1337/user/register/?id='+model.id+'&t='+model.password
+    text: 'http://localhost:1337/user/register/?id=' + model.id + '&t=' + model.password
   };
-  transporter.sendMail(mailOptions, function(error, info){
-    if(error){
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
       res.view('user/error', {message: 'При отправке письма произошла ошибка: ' + error.message});
     }
     //TODO: create different views for successful email sent after 1) registration, 2) repeated activation email request
